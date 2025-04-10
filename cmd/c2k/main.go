cb352abdb69d5ab86836ff2a9b53d43c24405c63
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2025-04-10 13:54:34 +0300

    Show version

--- cmd/c2k/main.go
+++ cmd/c2k/main.go
@@ -6,12 +6,25 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"runtime/debug"
+	"strings"
 )
+
+// TODO: Verbose mode is superset of include mode
+// TODO: Error reporting
+// TODO: README and Github fields
+
+var Version string
 
 func main() {
 	if len(os.Args) < 2 {
 		_, _ = fmt.Fprintf(os.Stderr, "Usage: c2k curl ...\n")
 		os.Exit(1)
+	}
+
+	if os.Args[1] == "-V" || os.Args[1] == "--version" || os.Args[1] == "version" {
+		fmt.Printf("Version: %s\n", getVersion())
+		os.Exit(0)
 	}
 
 	command, err := curl.ParseCommand(os.Args[1:])
@@ -32,4 +45,20 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getVersion() string {
+	if Version != "" {
+		return strings.Trim(Version, "\n\r")
+	}
+
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.revision" {
+				return fmt.Sprintf("dev-%s", setting.Value[:7])
+			}
+		}
+	}
+
+	return "dev"
 }
